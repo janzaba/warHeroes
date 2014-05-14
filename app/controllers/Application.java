8a7565591305152c9e974ec19a73493d4859dd60
Rafał Jóźwik <195933@student.pwr.wroc.pl>
2014-05-14 21:21:46 +0200

    Zmiany, ktore dorobilem jeszcze w KFC dotyczace widokow.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,7 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return TODO;
+        return ok(index.render("Dupa biskupa"));
     }
 
 }
